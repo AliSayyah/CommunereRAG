70847a55d8a080dac9c9b8b6c107ce0769760de8
alisayyah <ali.sayyah2@gmail.com>
2024-12-29 03:14:05 +0330

    added necessary fastapi dependencies

---
+++ CommunereRAG/services/chroma/dependency.py
@@ -0,0 +1,24 @@
+from typing import AsyncGenerator
+
+from chromadb import AsyncClientAPI, EmbeddingFunction
+from starlette.requests import Request
+from taskiq import TaskiqDepends
+
+
+async def get_chroma_client(
+    request: Request = TaskiqDepends(),
+) -> AsyncGenerator[AsyncClientAPI, None]:  # pragma: no cover
+    """
+    Returns Chroma client.
+
+    :param request: current request.
+    :returns:  chroma client.
+    """
+    return request.app.state.chroma_client
+
+
+async def get_ef(
+    request: Request = TaskiqDepends(),
+) -> AsyncGenerator[EmbeddingFunction, None]:  # pragma: no cover
+
+    return request.app.state.ef
