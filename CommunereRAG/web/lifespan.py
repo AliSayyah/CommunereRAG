343021697cba874b83bae00dcab3d0cdc8daa365
alisayyah <ali.sayyah2@gmail.com>
2024-12-29 03:12:46 +0330

    added fastapi startup setup for redis and chroma

--- CommunereRAG/web/lifespan.py
+++ CommunereRAG/web/lifespan.py
@@ -1,7 +1,10 @@
+import os
 from contextlib import asynccontextmanager
 from typing import AsyncGenerator
 
 import beanie
+import chromadb
+from chromadb.utils import embedding_functions
 from fastapi import FastAPI
 from motor.motor_asyncio import AsyncIOMotorClient
 from prometheus_fastapi_instrumentator.instrumentation import (
@@ -9,7 +12,9 @@ from prometheus_fastapi_instrumentator.instrumentation import (
 )
 
 from CommunereRAG.db.models import load_all_models
+from CommunereRAG.services.redis.lifespan import init_redis, shutdown_redis
 from CommunereRAG.settings import settings
+from CommunereRAG.tkq import broker
 
 
 async def _setup_db(app: FastAPI) -> None:
@@ -19,6 +24,21 @@ async def _setup_db(app: FastAPI) -> None:
         database=client[settings.db_base],
         document_models=load_all_models(),  # type: ignore
     )
+
+
+async def _setup_chroma(app: FastAPI) -> None:
+    openai_ef = embedding_functions.OpenAIEmbeddingFunction(
+        api_key=os.getenv("OPENAI_API_KEY"), model_name="text-embedding-3-small"
+    )
+    client = await chromadb.AsyncHttpClient(host="chroma")
+    app.state.ef = openai_ef
+    app.state.chroma_client = client
+
+
+def _setup_nltk(app: FastAPI) -> None:
+    import nltk
+
+    nltk.download("punkt_tab")
 
 
 def setup_prometheus(app: FastAPI) -> None:  # pragma: no cover
@@ -47,8 +67,16 @@ async def lifespan_setup(
     """
 
     app.middleware_stack = None
+    if not broker.is_worker_process:
+        await broker.startup()
     await _setup_db(app)
+    await _setup_chroma(app)
+    _setup_nltk(app)
+    init_redis(app)
     setup_prometheus(app)
     app.middleware_stack = app.build_middleware_stack()
 
     yield
+    if not broker.is_worker_process:
+        await broker.shutdown()
+    await shutdown_redis(app)
