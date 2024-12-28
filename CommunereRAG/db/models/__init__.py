bc81bdf2e6130c2dfa862b9cd624ac9088625af6
alisayyah <ali.sayyah2@gmail.com>
2024-12-29 03:15:35 +0330

    fix for router and models not loading correctly

--- CommunereRAG/db/models/__init__.py
+++ CommunereRAG/db/models/__init__.py
@@ -4,11 +4,11 @@ from typing import Sequence, Type
 
 from beanie import Document
 
-from CommunereRAG.db.models.dummy_model import DummyModel
+from CommunereRAG.db.models.document_model import DocumentRecord
 
 
 def load_all_models() -> Sequence[Type[Document]]:
     """Load all models from this folder."""
     return [
-        DummyModel,
+        DocumentRecord,
     ]
---
+++ CommunereRAG/web/api/upload/__init__.py
@@ -0,0 +1,5 @@
+"""Upload model API."""
+
+from CommunereRAG.web.api.upload.views import router
+
+__all__ = ["router"]
