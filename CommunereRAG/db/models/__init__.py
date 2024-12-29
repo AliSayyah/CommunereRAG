8e4c0d0dc7953c779bc4656349ad299bd6e17b8d
alisayyah <ali.sayyah2@gmail.com>
2024-12-29 15:10:39 +0330

    load `QueryLog`

--- CommunereRAG/db/models/__init__.py
+++ CommunereRAG/db/models/__init__.py
@@ -5,10 +5,12 @@ from typing import Sequence, Type
 from beanie import Document
 
 from CommunereRAG.db.models.document_model import DocumentRecord
+from CommunereRAG.db.models.query_model import QueryLog
 
 
 def load_all_models() -> Sequence[Type[Document]]:
     """Load all models from this folder."""
     return [
         DocumentRecord,
+        QueryLog
     ]
