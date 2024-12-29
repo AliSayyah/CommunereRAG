f707e1da301d0638a84da2f1a7a8710e43fa1a1a
alisayyah <ali.sayyah2@gmail.com>
2024-12-29 15:10:47 +0330

    added routers

--- CommunereRAG/web/api/router.py
+++ CommunereRAG/web/api/router.py
@@ -1,9 +1,11 @@
 from fastapi.routing import APIRouter
 
-from CommunereRAG.web.api import docs, upload, echo, monitoring
+from CommunereRAG.web.api import docs, upload, echo, monitoring, query, logs
 
 api_router = APIRouter()
 api_router.include_router(monitoring.router)
 api_router.include_router(docs.router)
 api_router.include_router(echo.router, prefix="/echo", tags=["echo"])
-api_router.include_router(upload.router, prefix="", tags=["upload"])
+api_router.include_router(upload.router, prefix="/upload", tags=["upload"])
+api_router.include_router(query.router, prefix="/query", tags=["query"])
+api_router.include_router(logs.router, prefix="/logs", tags=["logs"])
