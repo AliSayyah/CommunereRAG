0acd6b897990742f906e407be1d948719291d4d4
alisayyah <ali.sayyah2@gmail.com>
2024-12-29 15:07:57 +0330

    added retry functionality to taskiq

--- CommunereRAG/tkq.py
+++ CommunereRAG/tkq.py
@@ -1,7 +1,8 @@
 from typing import Any
 
 import taskiq_fastapi
-from taskiq import AsyncBroker, AsyncResultBackend, InMemoryBroker
+from taskiq import AsyncBroker, AsyncResultBackend, InMemoryBroker, \
+    SimpleRetryMiddleware
 from taskiq_redis import ListQueueBroker, RedisAsyncResultBackend
 
 from CommunereRAG.settings import settings
@@ -11,7 +12,9 @@ result_backend: AsyncResultBackend[Any] = RedisAsyncResultBackend(
 )
 broker: AsyncBroker = ListQueueBroker(
     str(settings.redis_url.with_path("/1")),
-).with_result_backend(result_backend)
+).with_result_backend(result_backend).with_middlewares(
+    SimpleRetryMiddleware(default_retry_count=3),
+)
 
 if settings.environment.lower() == "pytest":
     broker = InMemoryBroker()
